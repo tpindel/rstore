4f1abb7c94ac19eea40b549bbf1bada62d630828
tpindel <pindel.tomasz@gmail.com>
2013-03-02 11:25:12 +0100

    Rstore - init commit. Added Product model.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rstore::Application.config.secret_token = '310065b050fdb8456f0f848447f839f08b70834f1313bb950036e814c80afee5988c8e012102ddf06405fa0e5d659d9c6b9069e1de3b87d5e2abd1b03dcf52b1'
