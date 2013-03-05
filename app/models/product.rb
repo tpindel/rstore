3e7ae943a1c2d42e802fdad408e2da7ab7b60c03
tpindel <pindel.tomasz@gmail.com>
2013-03-05 21:08:03 +0100

    Shopping cart manangement - adding rpoducts, calculate total price, empty cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
 
   attr_accessible :description, :image_url, :price, :title
 
+  has_many :line_items
+
   validates :title, :price, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
@@ -10,4 +12,18 @@ class Product < ActiveRecord::Base
       :with    => %r{\.(gif|jpg|png)$}i,
       :message => 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item 
+  	if line_items.empty?
+		return true 
+	else
+    	errors.add(:base, 'Line Items present')
+		return false 
+	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rstore::Application.routes.draw do
+  resources :line_items
+
+
+  resources :carts
+
+
   get "store/index"
 
   resources :products
