2b7720fd66738d74084f980e506671b578adb8c1
tpindel <pindel.tomasz@gmail.com>
2013-03-09 14:12:55 +0100

    Capistrano config

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+set :application, 'rstore'
+set :domain, 'rstore.tpindel.pl'
+set :repository,  "git@github.com:tpindel/rstore.git"
+set :deploy_to, "/home/#{application}"
+
+
+# distribute your applications across servers (the instructions below put them
+# all on the same server, defined above as 'domain', adjust as necessary)
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+
+# miscellaneous options
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+
+namespace :deploy do
+  desc "cause Passenger to initiate a restart"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=dev"
+  end
+end
+
+after "deploy:update_code", :bundle_install
+desc "install the necessary prerequisites"
+task :bundle_install, :roles => :app do
+  run "cd #{release_path} && bundle install"
+end
